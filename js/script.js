0e37ad7f3e2014e9f0032d73f4cb25ab73e71859
Alireza Ahmand <mr.alireza.ahmand@gmail.com>
2021-11-17 06:53:48 +0330

    dependencies commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+function Run() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var JData = JSON.parse(this.responseText);
+            document.getElementById("caption").hidden = false;
+            document.getElementById("files").hidden = false;
+            var prettyCaption = JData.caption;
+            document.getElementById("caption").innerHTML = prettyCaption.replace('\n', '<br>');
+
+            if (isUnicode(document.getElementById('caption').textContent)) {
+                document.getElementById('caption').style.direction = 'rtl';
+            }
+            else {
+                document.getElementById('caption').style.direction = 'ltr';
+            }
+            if (JData.type == 'side') {
+                var count = 0;
+                JData.data.forEach(element => {
+                    let btn = document.createElement("a");
+                    btn.className = "btn p-1 m-1";
+                    btn.style.backgroundColor = "#00b0ff";
+                    btn.style.color = 'white';
+                    btn.innerHTML = "<i class=\"fas fa-download\"></i> " + count++;
+                    btn.href = element.file;
+                    btn.target = '_blank';
+                    document.getElementById('files').appendChild(btn);
+                });
+            } else {
+                let btn = document.createElement("a");
+                btn.className = "btn p-1 m-1";
+                btn.style.backgroundColor = "#00b0ff";
+                btn.style.color = 'white';
+                btn.innerHTML = "<i class=\"fas fa-download\"></i> Save";
+                btn.href = JData.file;
+                btn.target = '_blank';
+                document.getElementById('files').appendChild(btn);
+            }
+        }
+    };
+    xmlhttp.open("GET", "https://mr-alireza.ir/API/instagram.php?link=" + document.getElementById("url-field").value, true);
+    xmlhttp.send();
+}
+
+function isUnicode(str) {
+    var letters = [];
+    for (var i = 0; i <= str.length; i++) {
+        letters[i] = str.substring((i - 1), i);
+        if (letters[i].charCodeAt() > 255) { return true; }
+    }
+    return false;
+}
