c9f98f74d4499a0363d9b2e0c5426c05f642c780
Alireza Ahmand <mr.alireza.ahmand@gmail.com>
2022-01-26 18:35:53 +0330

    added support for stories

--- instagram.php
+++ instagram.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-type: application/json');
 $_COOKIE = '';
-error_reporting(0);
+//error_reporting(0);
 class Instagram
 {
     function igInfo($user)
@@ -94,10 +94,65 @@ class Instagram
             return json_encode(['status' => false], 128);
         }
     }
+    function igStory($url)
+    {
+        $urlnew = str_replace('?utm_medium=share_sheet', '/', $url);
+        $curl_id = curl_init();
+        curl_setopt_array($curl_id, array(
+            CURLOPT_URL => sprintf('%s?__a=1', $urlnew),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_COOKIEFILE => 'cookies.txt',
+            CURLOPT_COOKIEJAR => 'cookies.txt'
+        ));
+        $id = curl_exec($curl_id);
+        curl_close($curl_id);
+        $userid = json_decode($id)->user->id;
+        preg_match('/\/stories\/(.*?)\/(.*?)\//', $urlnew, $match);
+        $request = curl_init();
+        curl_setopt_array($request, array(
+            CURLOPT_URL => sprintf('https://i.instagram.com/api/v1/feed/user/%s/reel_media/', $userid),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2_0,
+            CURLOPT_COOKIEFILE => 'cookies.txt',
+            CURLOPT_COOKIEJAR => 'cookies.txt',
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'User-Agent: Mozilla/5.0 (Linux; Android 8.1.0; Pixel C Build/OPM8.190605.005; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.90 Safari/537.36 Instagram 179.0.0.31.132 Android (27/8.1.0; 320dpi; 1800x2448; Google/google; Pixel C; dragon; drago',
+                'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'Cache-Control: max-age=0',
+                'Cookie: ' . $_COOKIE
+            )
+        ));
+        $exec = curl_exec($request);
+        $response = json_decode($exec);
+        curl_close($request);
+        foreach ($response->items as $items) {
+            if ($items->pk == $match[2]) {
+                if ($items->media_type == 2) {
+                    return json_encode(['status' => true, 'type' => 'video', 'caption' => null, 'file' => $items->video_versions[0]->url], 128);
+                } elseif ($items->media_type == 1) {
+                    return json_encode(['status' => true, 'type' => 'image', 'caption' => null, 'file' => $items->image_versions2->candidates[0]->url], 128);
+                }
+            }
+        }
+    }
 }
 $instagram = new Instagram();
 if (isset($_GET['user']) && !empty($_GET['user'])) {
     echo $instagram->igInfo($_GET['user']);
 } elseif (isset($_GET['link']) && !empty($_GET['link'])) {
-    echo $instagram->igSave($_GET['link']);
+    if (str_contains($_GET['link'], 'stories')) {
+        echo $instagram->igStory($_GET['link']);
+    } else {
+        echo $instagram->igSave($_GET['link']);
+    }
 }
