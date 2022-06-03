8989c0aed9514c64ddd8d165a042d9b1c64f6604
Incognito-Coder <mr.alireza.ahmand@gmail.com>
2022-06-03 19:18:18 +0430

    rewrited from scratch! Instagram closed __a=1

--- instagram.php
+++ instagram.php
@@ -3,33 +3,19 @@ header('Content-type: application/json');
 header('Access-Control-Allow-Origin: *');
 $_COOKIE = 'mid=YpBUDwALAAFTg5TqZTL7LwgJbcHG; ig_did=D7A6DA5A-CD8E-4833-AB90-017C771A7040; ig_nrcb=1; csrftoken=tcL2KnLkTcZsut7DQ5wJ7eB0Q8FMdGuq; ds_user_id=31384858829; sessionid=31384858829%3AnT7bRJSjP7RgY6%3A21; rur="CLN\05431384858829\0541685162202:01f70da22413f9af74762e290946ffe51f97ab12fd3b955bf33929849f2dba09f1eb1f3a"';
 error_reporting(0);
+require 'v2/instagram.php';
+
+use InstagramPHP\InstagramReloaded;
+
 class Instagram
 {
     function igInfo($user)
     {
-        $request = curl_init();
-        curl_setopt_array($request, array(
-            CURLOPT_URL => sprintf('https://www.instagram.com/%s/?__a=1', $user),
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_SSL_VERIFYHOST => false,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2_0,
-            CURLOPT_COOKIEFILE => 'cookies.txt',
-            CURLOPT_COOKIEJAR => 'cookies.txt',
-            CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_HTTPHEADER => array(
-                'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36',
-                'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'Cache-Control: max-age=0',
-                'Cookie: ' . $_COOKIE
-            )
-        ));
-        $exec = curl_exec($request);
-        $response = json_decode($exec);
-        curl_close($request);
-        if ($exec != '{}') {
-            $user = $response->graphql->user;
+        $ig = new InstagramReloaded($_COOKIE);
+        $response = $ig->fetchProfile($user);
+        if ($response != '{}') {
+            $jayson = json_decode($response);
+            $user = $jayson->data->user;
             $json = [
                 "status" => true,
                 "name" => $user->full_name,
@@ -48,29 +34,11 @@ class Instagram
 
     function igSave($url)
     {
-        $request = curl_init();
-        curl_setopt_array($request, array(
-            CURLOPT_URL => sprintf('%s&__a=1', $url),
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_SSL_VERIFYHOST => false,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2_0,
-            CURLOPT_COOKIEFILE => 'cookies.txt',
-            CURLOPT_COOKIEJAR => 'cookies.txt',
-            CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_HTTPHEADER => array(
-                'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36',
-                'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'Cache-Control: max-age=0',
-                'Cookie: ' . $_COOKIE
-            )
-        ));
-        $exec = curl_exec($request);
-        $response = json_decode($exec);
-        curl_close($request);
-        if ($exec != '{}') {
-            $data = $response->items[0];
+        $ig = new InstagramReloaded($_COOKIE);
+        $response = $ig->fetchData($ig->getMediaID($url));
+        if ($response != '{}') {
+            $jayson = json_decode($response);
+            $data = $jayson->items[0];
             if ($data->media_type == 2) {
                 if (empty($data->caption->text))
                     return json_encode(['status' => true, 'type' => 'video', 'caption' => null, 'like' => $data->like_count, 'comment' => $data->comment_count, 'file' => $data->video_versions[0]->url], 128);
@@ -186,46 +154,12 @@ class Instagram
     {
         preg_match('/.*([?].*)/', $url, $match);
         $urlnew =  str_replace($match[1], '/', $url);
-        $curl_id = curl_init();
-        curl_setopt_array($curl_id, array(
-            CURLOPT_URL => sprintf('%s?__a=1', $urlnew),
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_COOKIEFILE => 'cookies.txt',
-            CURLOPT_COOKIEJAR => 'cookies.txt'
-        ));
-        $id = curl_exec($curl_id);
-        curl_close($curl_id);
-        $userid = json_decode($id)->user->id;
-        preg_match('/\/stories\/(.*?)\/(.*?)\//', $urlnew, $match);
-        $request = curl_init();
-        curl_setopt_array($request, array(
-            CURLOPT_URL => sprintf('https://i.instagram.com/api/v1/feed/user/%s/reel_media/', $userid),
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_SSL_VERIFYHOST => false,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2_0,
-            CURLOPT_COOKIEFILE => 'cookies.txt',
-            CURLOPT_COOKIEJAR => 'cookies.txt',
-            CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_HTTPHEADER => array(
-                'User-Agent: Mozilla/5.0 (Linux; Android 8.1.0; Pixel C Build/OPM8.190605.005; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.90 Safari/537.36 Instagram 179.0.0.31.132 Android (27/8.1.0; 320dpi; 1800x2448; Google/google; Pixel C; dragon; drago',
-                'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'Cache-Control: max-age=0',
-                'Cookie: ' . $_COOKIE
-            )
-        ));
-        $exec = curl_exec($request);
-        $response = json_decode($exec);
-        curl_close($request);
-        foreach ($response->items as $items) {
-            if ($items->pk == $match[2]) {
+        preg_match('/\/stories\/(.*?)\/(.*?)\//', $urlnew, $matches);
+        $ig = new InstagramReloaded($_COOKIE);
+        $response = $ig->fetchStory($ig->getReelsID($urlnew));
+        $jayson = json_decode($response);
+        foreach ($jayson->items as $items) {
+            if ($items->pk == $matches[2]) {
                 if ($items->media_type == 2) {
                     return json_encode(['status' => true, 'type' => 'video', 'caption' => null, 'file' => $items->video_versions[0]->url], 128);
                 } elseif ($items->media_type == 1) {
