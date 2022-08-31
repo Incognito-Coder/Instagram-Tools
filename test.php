d88e7612d5864b8a3527ebcaa1c6855ce60652d6
Incognito-Coder <mr.alireza.ahmand@gmail.com>
2022-08-31 21:00:42 +0430

    unlink cookies.txt

--- test.php
+++ test.php
@@ -4,5 +4,8 @@ require 'vendor/autoload.php';
 use GuzzleHttp\Client;
 use InstagramPHP\Login as InstagramPHPLogin;
 
+if (file_exists('cookies.txt')) {
+    unlink('cookies.txt');
+}
 $session = new InstagramPHPLogin(new Client(), 'user', 'pass');
 $session->SessionLogin();
