6c02f129a03fe94c7de0d5fabb230c8c9695061d
Incognito-Coder <mr.alireza.ahmand@gmail.com>
2023-01-20 11:04:28 +0330

    fixed api

--- proxy.php
+++ proxy.php
@@ -4,15 +4,8 @@ $media = $_POST['file'];
 
 function MediaProxy($file)
 {
-    $tmp = 'tmp.jpg';
-    file_put_contents($tmp, file_get_contents($file));
-    echo json_encode(['path' => FullPath($tmp)]);
+    file_put_contents('tmp.jpg', file_get_contents($file));
+    echo json_encode(['path' => 'tmp.jpg']);
 }
-function FullPath($name)
-{
-    $break = Explode('/', $_SERVER["SCRIPT_NAME"]);
-    $pfile = $break[count($break) - 1];
-    $spliter = explode($pfile, $_SERVER["SERVER_NAME"] . $_SERVER["SCRIPT_NAME"]);
-    return 'https://' . $spliter[0] . $name;
-}
+
 MediaProxy($media);
