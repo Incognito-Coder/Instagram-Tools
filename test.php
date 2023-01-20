20612dc08a680c6e73e725afb8bb8ea327fb5f72
Incognito-Coder <mr.alireza.ahmand@gmail.com>
2023-01-20 11:04:39 +0330

    fixed api mode

--- test.php
+++ test.php
@@ -7,5 +7,5 @@ use InstagramPHP\Login as InstagramPHPLogin;
 if (file_exists('cookies.txt')) {
     unlink('cookies.txt');
 }
-$session = new InstagramPHPLogin(new Client(), 'user', 'pass');
+$session = new InstagramPHPLogin(new Client(), 'a.k.47.official', 'cumron');
 $session->SessionLogin();
