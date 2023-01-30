4c4016fbe78284303ed4df846e7662957a19ea41
Incognito-Coder <mr.alireza.ahmand@gmail.com>
2023-01-30 22:53:50 +0330

    ui fixes and new features

--- index.php
+++ index.php
@@ -22,9 +22,8 @@ header('Expires: 0');
     <link rel="shortcut icon" type="image/x-icon" href="https://www.instagram.com/static/images/ico/favicon.ico/36b3ee2d91ed.ico">
     <link href="https://cdn.jsdelivr.net/gh/rastikerdar/vazir-font@v30.1.0/dist/font-face.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous" />
-
-    <link rel="manifest" href="/manifest.json">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="manifest" href="manifest.json">
     <link rel="stylesheet" href="css/styles.css" type="text/css" />
 </head>
 
@@ -36,6 +35,19 @@ header('Expires: 0');
             <button class="btn btn-primary" type="button" id="button-search" onclick="Run()">Search</button>
         </div>
         <br>
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" hidden>
+
+            <div class="carousel-inner" id="carousel-child">
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
         <img id="thumb" class="img-fluid shadow bg-light text-dark p-2 mb-2 rounded" hidden />
         <p id="caption" class="shadow bg-light text-dark p-2 rounded" hidden></p>
         <div id="files" class="shadow bg-light text-dark p-2 rounded" hidden>Files:
@@ -45,14 +57,13 @@ header('Expires: 0');
         <!-- Copyright -->
         <div class="text-center m-3 p-2" style="background-color: rgba(0, 0, 0, 0.2);">
             © 2021-2023 Copyright : <a class="text-white" href="https://mr-alireza.ir/">Alireza Ahmand</a> | Designed with <i class="fas fa-heart" style="color:red"></i> by
-            Me
+            Alireza
         </div>
         <!-- Copyright -->
     </footer>
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.6.0.min.js"></script>
     <script src="sw.js"></script>
     <script src="js/script.js"></script>
-
     <script>
         $("#button-search").click(function() {
             $("#url-field").val('');
@@ -89,6 +100,7 @@ header('Expires: 0');
             });
         }
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 
