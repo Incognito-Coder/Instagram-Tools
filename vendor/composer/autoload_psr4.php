1092cdffcd45e1d3beae0fe6b25aebc2e3dd172e
Incognito-Coder <mr.alireza.ahmand@gmail.com>
2023-05-15 10:21:05 +0330

    updated composer files

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,7 +6,7 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-factory/src', $vendorDir . '/psr/http-message/src'),
+    'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src', $vendorDir . '/psr/http-factory/src'),
     'Psr\\Http\\Client\\' => array($vendorDir . '/psr/http-client/src'),
     'InstagramPHP\\' => array($baseDir . '/Instagram'),
     'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInitd83d1dd46a08fa1b20c92194a20a84dc
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInitd83d1dd46a08fa1b20c92194a20a84dc::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequired83d1dd46a08fa1b20c92194a20a84dc($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitd83d1dd46a08fa1b20c92194a20a84dc::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequired83d1dd46a08fa1b20c92194a20a84dc($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
